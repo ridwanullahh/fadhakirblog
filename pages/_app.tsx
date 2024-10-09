339c69d231c98f68a0bb65c28cc6392311243e48
Robocoders <team@blackbox.ai>
2024-10-09 21:24:06 +0000

    Fixed issues with NextAuth and updated authentication routes.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <ThemeProvider>
         <Component {...pageProps} />
       </ThemeProvider>
-    </UserProvider>
+    </SessionProvider>
   );
 }
 
